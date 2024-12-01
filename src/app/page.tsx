06df7772929c56ed8bbb09676f2b92c22a37f8d7
mhasnain23 <tryfrelancer23@gmail.com>
2024-12-01 22:27:51 +0500

    nit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
-   <>
-   <h1 className="text-6xl">Helo World</h1>
-   </>
+    <>
+      <h1 className="text-6xl">Helo World</h1>
+      <h2 className="text-3xl text-orange-500">testting</h2>
+    </>
   );
 }
