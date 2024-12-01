511229bf1635431e9683c32ae19fdafe6fc690fd
Muhammad Inam <nudmaufon@gmail.com>
2024-12-01 22:50:22 +0500

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-6xl">Helo World</h1>
-      <h2 className="text-3xl text-orange-500">testting</h2>
+     
     </>
   );
 }
